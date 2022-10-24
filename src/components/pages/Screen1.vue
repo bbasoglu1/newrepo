0f25152e8496ff5d64a194326ad0c43026cf73cb
NUCAL <yagmur.sahin@nucal.com>
2022-10-24 14:25:54 -0400

    NUCAL-UI | Front-end application initial commit, powered by NUCAL development team.

---
+++ src/components/pages/Screen1.vue
@@ -0,0 +1,74 @@
+<template>
+  <NucGridPage v-bind:gap="{'col':0,'row':0}" v-bind:rows="[1]"
+    v-bind:schema="{'r0-c0':{'col':0,'row':0,'showIn':['xs','sm','md','lg'],'colSpan':1,'rowSpan':1}}"
+    v-bind:columns="[1]" overflow="auto" v-bind:designMode="false" v-bind:minNsLimit="816"
+    v-bind:style="{'background':'#FFFFFF','paddingTop':'0px','paddingLeft':'0px','paddingRight':'0px','paddingBottom':'0px'}">
+    <NucGridCell v-bind:wrap="true" v-bind:reverse="false" overflow="hidden" direction="column"
+      v-bind:showInDevice="['xs','sm','md','lg']" verticalAlignment="top" horizontalAlignment="left"
+      id="3e12c702-1673-43d8-a04f-30db63988e83" v-bind:col="0" v-bind:row="0" v-bind:colSpan="1" v-bind:rowSpan="1"
+      v-bind:marginTop="0" v-bind:marginLeft="0" v-bind:marginRight="0" v-bind:marginBottom="0"
+      v-bind:style="{'height':'auto','background':'transparent','paddingTop':'0px','borderColor':'','borderStyle':'solid','borderWidth':'0px','paddingLeft':'0px','borderRadius':'0px','paddingRight':'0px','paddingBottom':'0px','borderTopWidth':'0px','borderLeftWidth':'0px','borderRightWidth':'0px','borderBottomWidth':'0px','borderTopLeftRadius':'1px','borderTopRightRadius':'1px','borderBottomLeftRadius':'1px','borderBottomRightRadius':'1px'}"
+      slot="r0-c0" area="r0-c0">
+      <WAccordionCollapse tag="div" v-bind:pill="false" size="md" text="Accordion 1" color="" v-bind:isNav="false"
+        theme="theme--light01" v-bind:active="false" v-bind:appear="false"
+        content="I start opened because visible is true 
+
+ Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore VHS."
+        v-bind:squared="false" variant="info" v-bind:visible="true" content2="I am collapsible second content!"
+        accordion="" v-bind:isEnabled="true" background="" borderColor="rgb(210, 218, 236)" v-bind:hasAccordion="true"
+        ncTypography="body1" v-bind:resetTrigger="false" ncTooltipText="" textAccordion="Accordion 2"
+        verticalWidth="60px"
+        v-bind:nucalDataModel="{'type':'NSW','isList':false,'dynamic':false,'structure':[{'type':'string','propName':'content','displayName':'Content'},{'type':'string','propName':'text','displayName':'Text'}]}"
+        verticalHeight="60px" v-bind:appearAccordion="false" horizontalWidth="72.4479px"
+        contentAccordion="Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore VHS."
+        horizontalHeight="37.5347px" ncClickFontColor="#FFFFFF" ncFocusFontColor="#FFFFFF" ncHoverFontColor="#FFFFFF"
+        v-bind:visibleAccordion="false" ncClickBorderColor="#4285f4" ncDisableFontColor="#FFFFFF"
+        ncFocusBorderColor="#4285f4" ncHoverBorderColor="#4285f4" v-bind:controlOfToggleCard="false"
+        ncDisableBorderColor="transparent" ncClickBackgroundColor="#4285f4" ncFocusBackgroundColor="#4285f4"
+        ncHoverBackgroundColor="#4285f4" ncDisableBackgroundColor="#969696" position="absolute" top="27.62738362185229%"
+        left="25.90657894736842%" id="5cec9a60-1ad4-4c27-bb1e-06387d47b855"
+        v-bind:style="{'width':'408px','height':'','fontSize':'7.5px','maxWidth':'','minWidth':'','tabIndex':'0px','boxShadow':'','fontStyle':'normal','marginTop':'0px','maxHeight':'','minHeight':'','textAlign':'','fontFamily':'Roboto-Regular,Roboto','fontWeight':'','lineHeight':'','marginLeft':'0px','paddingTop':'0px','textShadow':'','visibility':'visible','borderStyle':'solid','marginRight':'0px','paddingLeft':'0px','marginBottom':'0px','paddingRight':'0px','paddingBottom':'0px','borderTopWidth':'0px','textDecoration':'none','borderLeftWidth':'0px','borderRightWidth':'0px','horizontalHeight':'18.76735px','borderBottomWidth':'0px','borderTopLeftRadius':'0px','borderTopRightRadius':'0px','borderBottomLeftRadius':'0px','borderBottomRightRadius':'0px','zIndex':2}"/>
+      <WBlockButton tag="button" href="" v-bind:pill="false" size="md" text="Block Level Button" type="button"
+        v-bind:block="true" color="" theme="theme--light01" v-bind:squared="false" variant="primary"
+        v-bind:isEnabled="true" background="" linkTarget="_self" ncLinkType="link" borderColor="007BFF"
+        ncTypography="body1" v-bind:resetTrigger="false" ncTooltipText="" verticalWidth="60px"
+        v-bind:nucalDataModel="{'type':'NSW','isList':false,'dynamic':false,'structure':[{'type':'string','propName':'text','displayName':'Value'}]}"
+        verticalHeight="60px" horizontalWidth="72.4479px" toForRouterLink="to" horizontalHeight="37.5347px"
+        ncClickFontColor="#FFFFFF" ncFocusFontColor="#FFFFFF" ncHoverFontColor="#FFFFFF" ncClickBorderColor="#007bff"
+        ncDisableFontColor="#FFFFFF" ncFocusBorderColor="#007bff" ncHoverBorderColor="#007bff"
+        ncDisableBorderColor="transparent" ncClickBackgroundColor="#007bff" ncFocusBackgroundColor="#007bff"
+        ncHoverBackgroundColor="#007bff" ncDisableBackgroundColor="#969696" position="absolute" top="45.60286008740542%"
+        left="0%" id="7e1f87ec-fa68-474c-969c-1be504ff5200"
+        v-bind:style="{'width':'','height':'','fontSize':'6px','maxWidth':'','minWidth':'','tabIndex':'0px','boxShadow':'','fontStyle':'normal','marginTop':'0px','maxHeight':'','minHeight':'','textAlign':'center','fontFamily':'OpenSans-Bold,Open Sans','fontWeight':'bold','lineHeight':'0.7','marginLeft':'0px','paddingTop':'','textShadow':'','visibility':'visible','borderStyle':'solid','marginRight':'0px','paddingLeft':'','marginBottom':'0px','paddingRight':'','paddingBottom':'','borderTopWidth':'0.5px','textDecoration':'none','borderLeftWidth':'0.5px','borderRightWidth':'0.5px','horizontalHeight':'18.76735px','borderBottomWidth':'0.5px','borderTopLeftRadius':'2px','borderTopRightRadius':'2px','borderBottomLeftRadius':'2px','borderBottomRightRadius':'2px','zIndex':3}"/>
+      <WButton tag="button" href="" v-bind:pill="false" size="md" text="Button" type="button" v-bind:block="false"
+        color="" theme="theme--light01" v-bind:squared="false" variant="primary" ncBgType="bgColor"
+        v-bind:isEnabled="true" background="" linkTarget="_self" ncLinkType="link" borderColor="#007bff"
+        ncTypography="body1" v-bind:resetTrigger="false" ncTooltipText="" verticalWidth="60px"
+        v-bind:nucalDataModel="{'type':'NSW','isList':false,'dynamic':false,'structure':[{'type':'string','propName':'text','displayName':'Value'}]}"
+        textDecoration="none" verticalHeight="60px" horizontalWidth="72.4479px" nucalScaleWidth="" toForRouterLink="to"
+        horizontalHeight="37.5347px" ncClickFontColor="#FFFFFF" ncFocusFontColor="#FFFFFF" ncHoverFontColor="#FFFFFF"
+        nucalScaleHeight="" ncClickBorderColor="#007BFF" ncDisableFontColor="#FFFFFF" ncFocusBorderColor="#007BFF"
+        ncHoverBorderColor="#007BFF" ncDisableBorderColor="transparent" ncClickBackgroundColor="#007BFF"
+        ncFocusBackgroundColor="#007BFF" ncHoverBackgroundColor="#007BFF" ncDisableBackgroundColor="#969696"
+        position="absolute" top="92.20708255181414%" left="30.699754901960784%"
+        id="13bbadbc-2ad6-4016-9fd2-d2b070552b53"
+        v-bind:style="{'width':'','height':'','fontSize':'6px','maxWidth':'','minWidth':'','tabIndex':'0px','boxShadow':'','fontStyle':'normal','marginTop':'','maxHeight':'','minHeight':'','textAlign':'center','fontFamily':'OpenSans-Bold,Open Sans','fontWeight':'normal','lineHeight':'0.7','marginLeft':'','paddingTop':'','textShadow':'','visibility':'visible','borderStyle':'solid','marginRight':'','paddingLeft':'','marginBottom':'','paddingRight':'','paddingBottom':'','borderTopWidth':'0.5px','textDecoration':'none','borderLeftWidth':'0.5px','borderRightWidth':'0.5px','horizontalHeight':'18.76735px','borderBottomWidth':'0.5px','borderTopLeftRadius':'2px','borderTopRightRadius':'2px','borderBottomLeftRadius':'2px','borderBottomRightRadius':'2px','zIndex':4}"/>
+    </NucGridCell>
+  </NucGridPage>
+</template>
+<script>
+export default {
+  name: 'Screen1',
+  data() {
+    return {};
+  },
+  props: {},
+  created() {},
+  mounted() {},
+  computed: {},
+  methods: {},
+  watch: {},
+};
+</script>
+
+<style scoped></style>
